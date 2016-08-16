b2640d67487fb294ef652d0d9a3dc7faab7fb786
Francis Kennet V. Lamayo <dansisabot@gmail.com>
2016-08-16 18:26:58 +0800

    chore: add intial task for shipit

--- shipitfile.js
+++ shipitfile.js
@@ -3,17 +3,19 @@ module.exports = (shipit) => {
 
 	shipit.initConfig( {
 		'default' : {
-			'workspace'     : '/tmp/github-monitor',
-			'deployTo'      : '/tmp/deploy_to',
+			'workspace'     : '/tmp/learn-seneca',
+			'deployTo'      : '~/learn-seneca',
 			'repositoryUrl' : 'https://github.com/danswater/learn-seneca.git',
 			'ignores'       : ['.git', 'node_modules'],
 			'rsync'         : ['--del'],
 			'keepReleases'  : 2,
-			'key'           : '/path/to/key',
+			'key'           : '~/.ssh/id_rsa',
 			'shallowClone'  : true
 		},
 		'staging' : {
 			'servers' : 'root@162.248.160.69'
 		}
 	} );
+
+	shipit.task( 'pwd', () => shipit.remote( 'pwd' ) );
 };
