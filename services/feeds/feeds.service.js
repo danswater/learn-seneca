b83036ab98b45a540e0c90213789c51aaa2aa795
Francis Kennet V. Lamayo <dansisabot@gmail.com>
2016-09-02 18:11:57 +0800

    chore: remove production condition

--- services/feeds/feeds.service.js
+++ services/feeds/feeds.service.js
@@ -3,11 +3,6 @@
 const seneca = require( 'seneca' );
 const config = require( './config' );
 
-let senecaConfig;
-if ( process.env.NODE_ENV === 'production' ) {
-	senecaConfig = config.SENECA_CONFIG( seneca );
-}
-
 function boot ( err ) {
 	if ( err ) {
 		return process.exit( !console.error( err ) );
